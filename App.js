53d99df9f21d6880404e18eb39a32fa7c83f6b3e
JAGDEEP-BARYAR <156022795+JAGDEEP-BARYAR@users.noreply.github.com>
2024-03-08 19:53:40 -0700

    LAB4

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import { useState } from 'react';
-import { SafeAreaView} from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import ToDoForm from './components/ToDoForm';
 import ToDoList from './components/ToDoList';
 
 function App() {
-    const [tasks, setTasks] = useState([
-        'Do laundry',
-        'Go to gym',
-        'Walk dog'
-    ]);
-    return (
-        <SafeAreaView>
-            <ToDoList tasks={tasks} />
-            <ToDoForm />
-        </SafeAreaView>
-    );
+  const [tasks, setTasks] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+
+  const addTask = (task) => {
+    setTasks([...tasks, task]); 
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ToDoList tasks={tasks} />
+      <ToDoForm addTask={addTask} />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
 
 export default App;
